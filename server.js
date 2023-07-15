ba9b4c3a578718b038653da7335666b18774fec3
jinta <terry0222@naver.com>
2023-07-16 04:32:45 +0900

    create 통합 테스트

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const routes = require("./src/routes/routes");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://my-database:27017/test");
+mongoose.connect("mongodb://localhost:27017/test");
 //미들웨어 순서주의
 app.use(express.json());
 const PORT = 8000;
@@ -16,5 +16,13 @@ app.post("/", (req, res) => {
   console.log(req.body);
 });
 app.use("/api/products", routes);
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
+// mongoose.disconnect();
+
+// app.listen(PORT, HOST);
+// console.log(`Running on http://${HOST}:${PORT}`);
+
+module.exports = app;
---
+++ src/test/integration/product.int.test.js
@@ -0,0 +1,22 @@
+const request = require("supertest");
+const app = require("../../../server");
+
+let newProduct = require("../data/new-product.json");
+
+it("POST /api/products", async () => {
+  const response = await request(app).post("/api/products").send(newProduct);
+  expect(response.statusCode).toBe(201);
+  expect(response.body.name).toBe(newProduct.name);
+  expect(response.body.description).toBe(newProduct.description);
+});
+it("should return 500 on post /api/products", async () => {
+  const response = await request(app)
+    .post("/api/products")
+    .send({ name: "phone" });
+  expect(response.statusCode).toBe(500);
+  console.log("rbod", response.body);
+  expect(response.body).toStrictEqual({
+    message:
+      "Product validation failed: description: Path `description` is required.",
+  });
+});
